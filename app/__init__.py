d9a9bfee5195757d970415a3437bc05ad939b22f
Andreas Reuter <andreas.reuter@fu-berlin.de>
2018-08-12 16:17:32 +0200

    Add error handling and filter features

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,12 @@ from app.models import CertificateTree, ResourceCertificate, Roa, Manifest, Crl
 #from app.analysis_api import analysis_api
 #from app.util import DateConverter
 
+def jsonify_or_error(obj, obj_name):
+    if obj:
+        return jsonify(obj.asdict())
+    else:
+        return jsonify({'error':True, 'description': "File doesn't exist", 'filename': obj_name})
+
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
@@ -28,7 +34,7 @@ def create_app(config_name):
         f = {'parent': parent_id, 'data': []}
         for c in rc:
             c = c.asdict()
-            d = {'id': c['id'], 'value': c['certificate_name'], 'webix_kids': c['has_kids'] }
+            d = {'id': c['id'], 'value': c['certificate_name'], 'webix_kids': c['has_kids'], 'mft_name': c['manifest']}
             f['data'].append(d)
         for roa in roas:
             roa = roa.asdict()
@@ -41,14 +47,14 @@ def create_app(config_name):
     def get_resource_certificate_meta(cert_name):
         rc = db_session.query(ResourceCertificate).filter(ResourceCertificate.certificate_name == cert_name).first()
         rc = rc.asdict()
-        d = {'id': rc['id'], 'value': rc['certificate_name'], 'webix_kids': rc['has_kids'] }
+        d = {'id': rc['id'], 'value': rc['certificate_name'], 'webix_kids': rc['has_kids'], 'mft_name': rc['manifest']}
         return jsonify(d)
 
 
     @app.route('/api/objects/ct/<name>')
     def get_certificate_tree(name):
         cert_tree = db_session.query(CertificateTree).filter(CertificateTree.tree_name == name).first()
-        return jsonify(cert_tree.asdict())
+        return jsonify_or_error(cert_name, name)
 
 
     @app.route('/api/objects/ct/all')
@@ -59,22 +65,53 @@ def create_app(config_name):
     @app.route('/api/objects/rc/<cert_name>')
     def get_resource_certificate(cert_name):
         rc = db_session.query(ResourceCertificate).filter(ResourceCertificate.certificate_name == cert_name).first()
-        return jsonify(rc.asdict())
+        return jsonify_or_error(rc, cert_name)
 
     @app.route('/api/objects/mft/<mft_name>')
     def get_manifest(mft_name):
         rc = db_session.query(Manifest).filter(Manifest.manifest_name == mft_name).first()
-        return jsonify(rc.asdict())
+        return jsonify_or_error(rc, mft_name)
 
     @app.route('/api/objects/crl/<crl_name>')
     def get_crl(crl_name):
         rc = db_session.query(Crl).filter(Crl.crl_name == crl_name).first()
-        return jsonify(rc.asdict())
+        return jsonify_or_error(rc, crl_name)
 
     @app.route('/api/objects/roa/<roa_name>')
     def get_roa(roa_name):
         rc = db_session.query(Roa).filter(Roa.roa_name == roa_name).first()
         return jsonify(rc.asdict())
+
+    @app.route('/api/filter/', methods = ['POST'])
+    def filter_objects():
+        content = request.get_json()
+        
+        filter_attr = content['filter_attribute']
+        filter_value = content['filter_value']
+        file_type = content['file_type']
+        cert_tree = content['tree_name']
+    
+        results = []
+        
+        if file_type == "cer" or file_type == "all":
+            query = db_session.query(ResourceCertificate.id, ResourceCertificate.certificate_name).filter(ResourceCertificate.certificate_tree == cert_tree)
+            if filter_attr == "filename":
+                query = query.filter(ResourceCertificate.certificate_name.like("%"+filter_value+"%"))
+
+            [results.append({'id':rc[0], 'value':rc[1]}) for rc in query.all()]
+            return jsonify(results)
+
+        return 'JSON posted\n'
+
+        # "Certificate Tree: (tree_name")
+        # "Parent object" : If null, only look at TA of cert_tree. if not null,
+        # only look at children of that parent object
+        # "File type: [.cer, .roa, or "both"]
+        # validation status: [passed, warning, error] (has ot be at least one)
+        # filename
+        # location (not yet)
+        # subject, issuer
+        # Resource (IP, ASN, Ip prefix)
 
     @app.teardown_appcontext
     def shutdown_session(exception=None):
